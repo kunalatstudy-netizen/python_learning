26c6980eeb987b4378485b61cea3e2cf0e7ac5a9
Kunal Rai <kunalatstudy@gmail.com>
2026-05-22 04:14:24 +0530

    revision done till day 7

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -28,5 +28,27 @@ scissors = '''
 '''
 game_images = [rock, paper, scissors]
 
-user_choice = int(input())
+computer_choice = random.choice(game_images)
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n Choose an option:"))
+user_choice_img = game_images[user_choice]
+
+print(computer_choice)
+print(f"Computer choise ")
+print(user_choice_img)
+print(f"user choice ")
+
+
+if user_choice_img == computer_choice:
+    print("Draw")
+elif user_choice_img == rock and computer_choice == paper:
+    print("you loose")
+elif user_choice_img == paper and computer_choice == scissors:
+    print("You loose")
+elif user_choice_img == scissors and computer_choice == rock:
+    print("you loose")
+
+else:
+    print("You win")
+
+
 
---
+++ Day 6/Functions/task.py
@@ -0,0 +1,4 @@
+def my_function():
+    print("hello world")
+    print("hello again")
+my_function()
