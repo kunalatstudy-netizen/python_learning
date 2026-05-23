ec1298eaafef5a3ab0ff45f9b7d4541fe7a1d67e
Kunal Rai <kunalatstudy@gmail.com>
2026-05-23 05:46:02 +0530

    just for fun

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -1,11 +1,25 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+#
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    encrypted_text = []
+    for i in original_text:
+        shift_number = alphabet.index(i) + shift_amount
+        if shift_number > 26:
+            shift_number -= 26
+            shifted_text = alphabet[shift_number]
+        else:
+            shifted_text = alphabet[shift_number]
+        encrypted_text.append(shifted_text)
+    return encrypted_text
+
+
+print(encrypt("kunaz", 4))
 
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,4 @@
+def my_function(name):
+    print(f"Hello {name}")
+
+my_function("alice")
